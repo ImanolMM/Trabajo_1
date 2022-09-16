5bf46bb6454b11a65624d4fa9a3e12cd6cdbf2b7
ImanolMM <imanolm.upv@gmail.com>
2022-09-16 10:38:58 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    private ArrayList<Web> webs;
+    private ArrayList <PClave> palabras;
+    private boolean cargado;
+
+    public Main (){
+        this.webs = new ListaWeb();
+        this.palabras = new ListaPClave();
+        this.cargado = false;
+    }
     public static void main (String [] args){
         System.out.println("hola");
         //hola

ab784e2dcf678a8011738b36e785372438788a4a
ImanolMM <imanolm.upv@gmail.com>
2022-10-04 11:11:18 +0200

    modulos de cargar

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,48 +21,58 @@ public class Main{
             palabrasMap = new HashPalabras();
             webRelacionadas = new ListaWebRelacionadas();
 
-            String linea;
-            BufferedReader entrada = new BufferedReader(new FileReader(System.getProperty("user.dir")+"\\src\\" + nomClave));
-
-            while ((linea = entrada.readLine()) != null){ // loop de cargar palabras
-                palabrasMap.añadirPClave( linea, new PClave(linea));
-            }
-
-            entrada = new BufferedReader(new FileReader(System.getProperty("user.dir")+"\\src\\" + nomWeb));
-
-            int prueba = 0;
-            while ((linea = entrada.readLine()) != null){ //loop de cargar webs
-                String []partes = linea.split(":");
-                webs.añadirWeb(partes[1], Integer.parseInt(partes [0]));
-            }
-
-            entrada = new BufferedReader(new FileReader(System.getProperty("user.dir")+"\\src\\" + nomRelaciones));
-
-            prueba= 0;// para pruebas
-            while ((linea = entrada.readLine()) != null){ // loop de cargar las relaciones de webs
-                String [] partes = linea.split(" ---> ");
-                //System.out.println(prueba);
-
-                if (partes.length > 1) {
-                    linea = partes [1];
-                    partes = linea.split(" ### ");
-                    webRelacionadas.añadirWebRelacionadas(partes);
-                }
-                else{
-                    webRelacionadas.añadirWebRelacionadas(null);
-                }
-                prueba++;
-            }
-            entrada.close();
+            cargarPalabras(nomClave);
+            cargarWebs(nomWeb);
+            cargarWebrelacionadas(nomRelaciones);
 
             cargarRelacionesWebs();
             cargarRelacionesPalabras();
 
         }
-        catch (IOException e){e.printStackTrace();} catch (DosWebsConMismoIdException e) {e.printStackTrace();}
+        catch (IOException e){e.printStackTrace();}
 
     }
+    public static void cargarPalabras (String nomClave) throws IOException {
+        String linea;
+        BufferedReader entrada = new BufferedReader(new FileReader(System.getProperty("user.dir")+"\\src\\" + nomClave));
 
+        while ((linea = entrada.readLine()) != null){ // loop de cargar palabras
+
+            palabrasMap.añadirPClave( linea, new PClave(linea));
+        }
+        entrada.close();
+    }
+    public static void cargarWebs (String nomWeb) throws IOException {
+        String linea;
+        BufferedReader entrada = new BufferedReader(new FileReader(System.getProperty("user.dir")+"\\src\\" + nomWeb));
+
+        while ((linea = entrada.readLine()) != null){ //loop de cargar webs
+            try{
+                String []partes = linea.split(":");
+                webs.añadirWeb(partes[1], Integer.parseInt(partes [0]));
+
+            } catch (DosWebsConMismoIdException e) {e.printStackTrace();}
+        }
+        entrada.close();
+    }
+    public static void cargarWebrelacionadas (String nomRelaciones) throws IOException {
+        String linea;
+        BufferedReader entrada = new BufferedReader(new FileReader(System.getProperty("user.dir")+"\\src\\" + nomRelaciones));
+
+        while ((linea = entrada.readLine()) != null){ // loop de cargar las relaciones de webs
+            String [] partes = linea.split(" ---> ");
+
+            if (partes.length > 1) {
+                linea = partes [1];
+                partes = linea.split(" ### ");
+                webRelacionadas.añadirWebRelacionadas(partes);
+            }
+            else{
+                webRelacionadas.añadirWebRelacionadas(null);
+            }
+        }
+        entrada.close();
+    }
     public static void cargarRelacionesWebs (){
         String [] idWebs;
         int ind,cont =0;
@@ -94,13 +105,19 @@ public class Main{
         }
     }
 
-    public HashWeb devolverWebs (){ //para los JUnits
+    protected HashWeb devolverWebs (){ //para los JUnits
         return webs;
     }
 
     protected ListaWebRelacionadas devolverWebRelacionadas (){ //para los JUnits
         return webRelacionadas;
     }
+
+    protected HashPalabras obtenerPClave (){ //para los JUnits
+        return palabrasMap;
+    }
+
+
     public static void prueba(){
         cargarArchivos("words.txt","index-2022-2023","pld-arcs-1-N-2022-2023");
     }
